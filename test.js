4ccc340f70b5ecce31d27889c52385031af71be8
nathanieljamesbaird-dev <nathanieljamesbaird@gmail.com>
2026-03-01 14:49:21 +1030

    Add files via upload

---
+++ test.js
@@ -0,0 +1,20 @@
+// script.js
+
+// 1. Select the button element by its ID
+const myButton = document.getElementById('myButton');
+const outputElement = document.getElementById('output');
+
+// 2. Add a 'click' event listener
+myButton.addEventListener('click', function() {
+
+});
+
+// // script.js
+
+// // 1. Select the button element by its ID
+// const myButton = document.getElementById('myButton');
+
+// // 2. Assign an anonymous function to the onclick property
+// myButton.onclick = function() {
+//   alert("The button was clicked using onclick property!");
+// };
